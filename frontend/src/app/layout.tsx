e8226eee16a697c5473ed8efab0b26b8cd70c645
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-11-23 22:17:55 +0900

    コメントアウト削除して再push

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import { Amplify } from "aws-amplify";
-// import outputs from "../../../amplify_outputs.json";
+import outputs from "../../../amplify_outputs.json";
 
-// Amplify.configure(outputs);
+Amplify.configure(outputs);
 
 import { ReactNode } from "react";
 
