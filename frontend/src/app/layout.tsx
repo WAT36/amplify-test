ff1c31b2e267f45955f3e7bb6eded9c2459063f2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-11-23 22:27:50 +0900

    amplify_output

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../../../amplify_outputs.json";
+import outputs from "../../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
